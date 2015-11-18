24aac9d7d332b1a2cbb231ccddd1445ad1907955
Kenny Younger <kyounger@gmail.com>
2015-11-18 04:25:23 -0600

    Add Lightroom symlink install script

---
+++ Lightroom/install.sh
@@ -0,0 +1,3 @@
+ln -s ~/Dropbox/Lightroom ~/Pictures/Lightroom
+ln -s ~/Dropbox/Lightroom/LightroomApplicationSupport ~/Library/Application\ Support/Adobe/Lightroom
+ln -s ~/Dropbox/Lightroom/com.adobe.Lightroom6.plist ~/Library/Preferences/com.adobe.Lightroom6.plist
