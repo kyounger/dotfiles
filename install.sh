1144bbbea704ae61446d2af1757e60cb7753db55
Kenny Younger <kyounger@gmail.com>
2017-08-02 08:56:14 -0500

    Use MACHINE_CONFIG_TYPE env variable to determine which brew file to append; move xcode into work/home subfiles

--- install.sh
+++ install.sh
@@ -1,59 +1,66 @@
-cd ~
+echo Home is: $HOME
+
+cd $HOME
 
 #rm -f bin
-ln -fs .dotfiles/bin
+ln -fs $HOME/.dotfiles/bin
 
 #rm -f .gitignore
-ln -fs .dotfiles/git/.gitignore
+ln -fs $HOME/.dotfiles/git/.gitignore
 
 #rm -f .gitconfig
-ln -fs .dotfiles/git/.gitconfig
+ln -fs $HOME/.dotfiles/git/.gitconfig
 
 #rm -f .jshintrc
-ln -fs .dotfiles/jshint/.jshintrc
+ln -fs $HOME/.dotfiles/jshint/.jshintrc
 
 #rm -f .sqliterc
-ln -fs .dotfiles/sqlite/.sqliterc
+ln -fs $HOME/.dotfiles/sqlite/.sqliterc
 
 #rm -f .tmux.conf
-ln -fs .dotfiles/tmux/.tmux.conf
+ln -fs $HOME/.dotfiles/tmux/.tmux.conf
 
 #rm -f .gvimrc
-ln -fs .dotfiles/vim/.gvimrc
+ln -fs $HOME/.dotfiles/vim/.gvimrc
 
 #rm -f .vimrc
-ln -fs .dotfiles/vim/.vimrc
+ln -fs $HOME/.dotfiles/vim/.vimrc
 
 #rm -f .vim
-ln -fs .dotfiles/vim/.vim
+ln -fs $HOME/.dotfiles/vim/.vim
 
 #rm -f .vim
-ln -fs .dotfiles/vim/.ideavimrc
+ln -fs $HOME/.dotfiles/vim/.ideavimrc
 
 #rm -f .zlogin
-ln -fs .dotfiles/zsh/.zlogin
+ln -fs $HOME/.dotfiles/zsh/.zlogin
 
 #rm -f .zlogout
-ln -fs .dotfiles/zsh/.zlogout
+ln -fs $HOME/.dotfiles/zsh/.zlogout
 
 #rm -f .zprezto
-ln -fs .dotfiles/zsh/.zprezto
+ln -fs $HOME/.dotfiles/zsh/.zprezto
 
 #rm -f .zpreztorc
-ln -fs .dotfiles/zsh/.zpreztorc
+ln -fs $HOME/.dotfiles/zsh/.zpreztorc
 
 #rm -f .zprofile
-ln -fs .dotfiles/zsh/.zprofile
+ln -fs $HOME/.dotfiles/zsh/.zprofile
 
 #rm -f .zshenv
-ln -fs .dotfiles/zsh/.zshenv
+ln -fs $HOME/.dotfiles/zsh/.zshenv
 
 #rm -f .zshrc
-ln -fs .dotfiles/zsh/.zshrc
+ln -fs $HOME/.dotfiles/zsh/.zshrc
 
-mkdir -p .brewfile
-ln -fs .dotfiles/Brewfile ~/.brewfile/Brewfile
+mkdir -p $HOME/.brewfile
+ln -fs $HOME/.dotfiles/Brewfile $HOME/.brewfile/Brewfile
+ln -fs $HOME/.dotfiles/Brewfile.work $HOME/.brewfile/Brewfile.work
+ln -fs $HOME/.dotfiles/Brewfile.home $HOME/.brewfile/Brewfile.home
 
 #run .osx defaults
-~/.dotfiles/mathiasbynens-dotfiles/.osx
+$HOME/.dotfiles/mathiasbynens-dotfiles/.osx
 
+sudo sh -c "echo '#ensure this is set properly for this machine' > /etc/zshenv"
+sudo sh -c "echo 'export MACHINE_CONFIG_TYPE=home' >> /etc/zshenv"
+sudo vim /etc/zshenv
