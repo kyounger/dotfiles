d2cab7278fa5b5ecd1d6e8815be77a52dcb7e299
Kenny Younger <kyounger@gmail.com>
2017-12-19 12:41:56 -0600

    Only use the ls config we want

--- aliases.zsh
+++ aliases.zsh
@@ -95,39 +95,13 @@ alias l='/usr/local/bin/gls -al --group-directories-first --color=always'
 alias lh='ls -ald .*'
 alias lsd3='du -sk * | sort -nr | head -3'
 
-# ls
-if is-callable 'dircolors'; then
-  # GNU Core Utilities
-  alias ls='ls --group-directories-first'
 
-  if zstyle -t ':prezto:module:utility:ls' color; then
-    if [[ -s "$HOME/.dir_colors" ]]; then
-      eval "$(dircolors --sh "$HOME/.dir_colors")"
-    else
-      eval "$(dircolors --sh)"
-    fi
-
-    alias ls="${aliases[ls]:-ls} --color=auto"
-  else
-    alias ls="${aliases[ls]:-ls} -F"
-  fi
-else
-  # BSD Core Utilities
-  if zstyle -t ':prezto:module:utility:ls' color; then
-    # Define colors for BSD ls.
-    export LSCOLORS='exfxcxdxbxGxDxabagacad'
-    export LSCOLORS='DxfxcxdxbxGxDxabagacad'
-
-
-    # Define colors for the completion system.
-    export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
-    export LS_COLORS='di=93:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
-
-    alias ls="${aliases[ls]:-ls} -G"
-  else
-    alias ls="${aliases[ls]:-ls} -F"
-  fi
-fi
+# Define ls colors
+export LSCOLORS='exfxcxdxbxGxDxabagacad'
+export LSCOLORS='DxfxcxdxbxGxDxabagacad'
+# Define ls colors for the completion system
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
+export LS_COLORS='di=93:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
 
 # change dir (or make and change)
 alias u='cd ..'
