10794383158c6a62369c1f64af536ae542ee231a
Kenny Younger <kyounger@gmail.com>
2018-07-28 07:52:39 -0500

    BTT config management

---
+++ BetterTouchTool/install.sh
@@ -0,0 +1,3 @@
+rm -rf ~/Library/Application\ Support/BetterTouchTool
+ln -s ~/.dotfiles/BetterTouchTool/BetterTouchToolApplicationSupport/ ~/Library/Application\ Support/BetterTouchTool
+ln -sf ~/.dotfiles/BetterTouchTool/com.hegenberg.BetterTouchTool.plist ~/Library/Preferences/com.hegenberg.BetterTouchTool.plist
