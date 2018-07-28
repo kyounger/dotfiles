57ede7b8b1e083fd494d24143e7e2638f465ad94
Kenny Younger <kyounger@gmail.com>
2018-07-28 07:42:05 -0500

    Fix lightroom install

--- Lightroom/install.sh
+++ Lightroom/install.sh
@@ -1,3 +1,2 @@
 ln -s ~/Dropbox/Lightroom ~/Pictures/Lightroom
 ln -s ~/Dropbox/Lightroom/LightroomApplicationSupport ~/Library/Application\ Support/Adobe/Lightroom
-ln -s ~/Dropbox/Lightroom/com.adobe.Lightroom6.plist ~/Library/Preferences/com.adobe.Lightroom6.plist
