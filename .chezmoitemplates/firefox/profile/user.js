4e65788d8348f94732d74dc411e021fb04aac2db
kyounger <kyounger@gmail.com>
2022-03-26 08:45:45 -0500

    More FF user.js config

--- .chezmoitemplates/firefox/profile/user.js
+++ .chezmoitemplates/firefox/profile/user.js
@@ -1,1 +1,5 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref('browser.aboutConfig.showWarning', false);
+user_pref('browser.download.lastDir.savePerSite', true);
+user_pref('findbar.highlightAll', true);
+user_pref('browser.toolbars.bookmarks.visibility', 'never');
