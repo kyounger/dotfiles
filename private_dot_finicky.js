baadfaff00cc3158c7d4339f6b9c9999650771e1
kyounger <kenny@sheerfocus.com>
2022-10-02 13:46:14 -0500

    Finicky config

---
+++ private_dot_finicky.js
@@ -0,0 +1,8 @@
+// TODO: Look into containers support for this: https://github.com/johnste/finicky/issues/211
+module.exports = {
+  defaultBrowser: "Firefox",
+  options: {
+    hideIcon: false, 
+    checkForUpdate: true,
+  },
+};
