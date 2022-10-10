c20e9058a78ec498b4f23c86200a30798a88eba8
kyounger <kyounger@cloudbees.com>
2022-10-10 18:29:00 -0500

    Use brave as default

--- private_dot_finicky.js
+++ private_dot_finicky.js
@@ -1,6 +1,7 @@
 // TODO: Look into containers support for this: https://github.com/johnste/finicky/issues/211
+// TODO: if sources is iTerm, always ask
 module.exports = {
-  defaultBrowser: "Firefox",
+  defaultBrowser: "Brave Browser",
   options: {
     hideIcon: false, 
     checkForUpdate: true,
