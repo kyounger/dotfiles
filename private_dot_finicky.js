9d4a96a3dff8ada52c47fb76231f49371b75d5f4
Kenny Younger <kyounger@gmail.com>
2023-11-23 19:21:28 -0600

    final finicky

--- private_dot_finicky.js
+++ private_dot_finicky.js
@@ -1,7 +1,6 @@
 // TODO: Look into containers support for this: https://github.com/johnste/finicky/issues/211
-// TODO: if sources is iTerm, always ask
 module.exports = {
-  defaultBrowser: "Brave Browser",
+  defaultBrowser: "Firefox",
   options: {
     hideIcon: false, 
     checkForUpdate: true,
@@ -21,13 +20,16 @@ module.exports = {
     {
       // You can get the path of the process that triggered Finicky (EXPERIMENTAL)
       match: ({ opener }) => {
-        return opener.path && opener.path.startsWith("/Applications/iTerm.app")
+        return opener.path && (
+          opener.path.startsWith("/Applications/iTerm.app") ||
+          opener.path.startsWith("/Applications/Slack.app")
+        )
       },
       browser: "Browserosaurus"
     },
     {
       // You can get the path of the process that triggered Finicky (EXPERIMENTAL)
-      match: ({ opener }) => opener.path && opener.path.startsWith("/Applications/SketchUp 2022/SketchUp.app"),
+      match: ({ opener }) => opener.path && opener.path.startsWith("/Applications/SketchUp"),
       browser: "Browserosaurus"
     },
   ]
